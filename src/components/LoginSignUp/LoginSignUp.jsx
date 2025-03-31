de91b0c8c81c46b614227f7636164ccc5878b002
iLeonard0 <folleleonardoprogramming@gmail.com>
2025-03-31 20:41:32 -0300

    feat: finishing the implementation of the login screen.

--- src/components/LoginSignUp/LoginSignUp.jsx
+++ src/components/LoginSignUp/LoginSignUp.jsx
@@ -1,34 +1,85 @@
-import React from "react";
-import { Box, Input, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import "./LoginSignUp.css";
+import logo from "/public/letiq-logo.png";
 
 export default function LoginSignUp() {
+  const [nickname, setNickname] = useState("");
+
+  const onKeyDown = (event) => {
+    if (event.key === "Enter" && nickname.trim()) {
+      saveUsername();
+    }
+  };
+
+  function saveUsername() {
+    console.log("Nome salvo:", nickname);
+  }
+
   return (
-    <Box>
-      <Box sx={{ display: 'flex', alignContent:'center', flexWrap: 'wrap', flexDirection: 'column'}}>
-        <Typography variant="h6" sx={{display: 'flex', justifyContent: 'center', p:1 }}>
-          Cadastre-se
-      </Typography>
-        <TextField
-          required
-          label="Username"
-          variant="outlined"
-          sx={{
-            width: '100%',
-            maxWidth: '450px'
-            }}>
-        </TextField>
-        <TextField
-          required
-          type="password"
-          variant="outlined"
-          label="Password"
-          sx={{
-            marginTop: 2,
-            width: '100%',
-            maxWidth: '450px'
-          }}>
-        </TextField>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundColor: "#dcdcdc",
+      }}
+    >
+      <Box>
+          <img src={logo} alt="Logo" style={{width: '300px', height: '320px'}}/> 
       </Box>
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: "450px",
+        }}
+      >
+        <TextField
+          label="Digite seu apelido"
+          variant="outlined"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+          onKeyDown={onKeyDown}
+          className="custom-textfield"
+          sx={{
+            color: "black",
+            width: "100%",
+            maxWidth: "450px",
+            borderRadius: "15px",
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "15px",
+            },
+          }}
+        />
+      </Box>
+      {nickname.trim() && (
+        <Box
+          className="fade-in"
+          width="100%"
+          maxWidth="450px"
+          display="flex"
+          justifyContent="center"
+        >
+          <Button
+            onClick={saveUsername}
+            sx={{
+              backgroundColor: "#21399b",
+              marginTop: 1.5,
+              width: "100%",
+              maxWidth: "450px",
+              borderRadius: "15px",
+              "& .MuiOutlinedInput-root": {
+                borderRadius: "15px",
+              },
+            }}
+            variant="contained"
+          >
+            Começar
+          </Button>
+        </Box>
+      )}
     </Box>
-  )
+  );
 }
