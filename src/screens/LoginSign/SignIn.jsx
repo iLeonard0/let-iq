cd5ebacb90cb9d2cba4c2975af51bbed4e344613
EduardoFassini <e.fassini@edu.unipar.br>
2025-05-15 21:51:18 -0300

    Ajustes da tela de login, de cadastro, novo dialog para redefinir senha, e funcões de integração com firebase relacionadas

--- src/screens/LoginSign/SignIn.jsx
+++ src/screens/LoginSign/SignIn.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
-import { Box, Button, TextField, Typography, Link, IconButton, InputAdornment, Paper } from "@mui/material";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  IconButton,
+  InputAdornment,
+  Paper,
+} from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import "./SignIn.css";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/header/Header";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../services/firebase";
+import DialogChangePassword from "./DialogChangePassword/DialogChangePassword";
+import { loginWithGoogle } from "../../services/authService";
 
 export default function LoginSignUp() {
   const [nickname, setNickname] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [open, setOpen] = useState(false);
+
   const navigate = useNavigate();
 
   const togglePasswordVisibility = () => {
@@ -22,11 +34,34 @@ export default function LoginSignUp() {
     try {
       await signInWithEmailAndPassword(auth, nickname, password);
       console.log("Login bem-sucedido!");
-      navigate("/screens/QuizEditor/QuizEditor"); 
+      navigate("/screens/Spin/Spin");
     } catch (error) {
       console.error("Erro ao fazer login:", error);
-      alert("Erro ao fazer login. Verifique suas credenciais e tente novamente.");
+      alert(
+        "Erro ao fazer login. Verifique suas credenciais e tente novamente. ", error
+      );
     }
+  };
+  
+  const handleGoogleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await loginWithGoogle();
+      navigate("/screens/Spin/Spin");
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      alert(
+        "Erro ao fazer login. Verifique suas credenciais e tente novamente. ", error
+      );
+    }
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   const isButtonDisabled = !(nickname.trim() && password.trim());
@@ -40,10 +75,11 @@ export default function LoginSignUp() {
           justifyContent: "center",
           alignItems: "center",
           flexDirection: "column",
-          minHeight: "90vh",
+          minHeight: "100svh",
           textAlign: "center",
           backgroundColor: "#f5f5f5",
           padding: 2,
+          pb: 0,
         }}
       >
         <Paper
@@ -59,14 +95,20 @@ export default function LoginSignUp() {
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
+            pb: 1,
+            mt: '5%',
           }}
         >
-          <Typography variant="h1" gutterBottom sx={{
-            fontSize: "2rem",
-            lineHeight: "2rem",
-            fontWeight: "700",
-            textAlign: "center",
-          }}>
+          <Typography
+            variant="h1"
+            gutterBottom
+            sx={{
+              fontSize: "2rem",
+              lineHeight: "2rem",
+              fontWeight: "700",
+              textAlign: "center",
+            }}
+          >
             Fazer Login
           </Typography>
           <Box
@@ -76,13 +118,16 @@ export default function LoginSignUp() {
               marginTop: 4,
             }}
           >
-            <Typography variant="h6" gutterBottom sx={{
-              display: "flex",
-              fontWeight: "bold",
-              marginBottom: 0,
-            }}>
+            <Typography
+              variant="h6"
+              gutterBottom
+              sx={{
+                display: "flex",
+                fontWeight: "bold",
+                marginBottom: 0,
+              }}
+            >
               Nome de usuário
-
             </Typography>
             <TextField
               variant="outlined"
@@ -97,13 +142,16 @@ export default function LoginSignUp() {
                 },
               }}
             />
-            <Typography variant="h6" gutterBottom sx={{
-              display: "flex",
-              fontWeight: "bold",
-              marginBottom: 0,
-            }}>
+            <Typography
+              variant="h6"
+              gutterBottom
+              sx={{
+                display: "flex",
+                fontWeight: "bold",
+                marginBottom: 0,
+              }}
+            >
               Senha
-
             </Typography>
             <TextField
               type={showPassword ? "text" : "password"}
@@ -133,15 +181,21 @@ export default function LoginSignUp() {
               display: "flex",
               justifyContent: "space-between",
               marginBottom: 2,
-              marginTop: 0
+              marginTop: 0,
             }}
           >
-            <Link href="/screens/LoginSignUp/LoginSignUp" underline="hover">
-              Forgot your password?
-            </Link>
+            <Typography underline="hover" >
+              Esqueceu sua senha?
+              <Button 
+                style={{ marginLeft: "5px", textTransform: "none", fontSize: '1rem', }} 
+                onClick={handleOpen}
+              >
+                Redefinir Senha...
+              </Button>
+            </Typography>
           </Box>
           <Button
-            onClick={handleLogin} 
+            onClick={handleLogin}
             disabled={isButtonDisabled}
             sx={{
               backgroundColor: isButtonDisabled ? "#d3d3d3" : "#21399b",
@@ -152,16 +206,46 @@ export default function LoginSignUp() {
               fontSize: "1rem",
               fontWeight: "bold",
               textTransform: "none",
-              transition: "background-color 0.3s ease",
+              transition: "transform 0.2s cubic-bezier(.4,2,.6,1)",
               "&:hover": {
                 backgroundColor: isButtonDisabled ? "#d3d3d3" : "#1a2e7b",
+                transform: "scale(1.05)",
               },
             }}
-            variant="contained"
+            variant="contained"  
           >
             Fazer Login
           </Button>
-          <Box sx={{marginTop: 3}}>
+          <Box
+            sx={{
+              position: "relative",
+              width: "100%",
+              textAlign: "center",
+              marginY: 2,
+              mt: 3,
+            }}
+          >
+            <hr
+              style={{
+                border: "none",
+                borderTop: "1px solid rgb(170, 170, 170)",
+                margin: 0,
+              }}
+            />
+            <span
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                backgroundColor: "#fff", // ou a cor do fundo da sua tela
+                padding: "0 8px",
+              }}
+            >
+              ou
+            </span>
+          </Box>
+          <Box sx={{ marginTop: 1, width: "100%" }}>
             <Button
               sx={{
                 display: "flex",
@@ -180,12 +264,14 @@ export default function LoginSignUp() {
                 fontWeight: "500",
                 textTransform: "none",
                 fontSize: "1rem",
-                transition: "background-color 0.3s ease, box-shadow 0.3s ease",
+                transition: " transform 0.2s cubic-bezier(.4,2,.6,1)",
                 "&:hover": {
                   backgroundColor: "rgb(245, 245, 245)",
                   boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+                  transform: "scale(1.05)",
                 },
               }}
+              onClick={handleGoogleLogin}
             >
               <img
                 src="/public/logo-google-login.png"
@@ -195,7 +281,32 @@ export default function LoginSignUp() {
               Continuar com o Google
             </Button>
           </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: 2,
+              marginTop: 0,
+            }}
+          >
+            <Typography sx={{ mt: 2 }} underline="hover">
+              Não tem uma conta? 
+              <Button 
+                style={{ marginLeft: "5px", textTransform: "none", fontSize: '1rem' }} 
+                href="/screens/LoginSignUp/LoginSignUp"
+              >
+                Inscrever-se
+              </Button>
+            </Typography>
+          </Box>
         </Paper>
+
+        <DialogChangePassword
+          onClose={handleClose}
+          open={open}
+          fullWidth
+          maxWidth="sm"
+        />
       </Box>
     </>
   );
